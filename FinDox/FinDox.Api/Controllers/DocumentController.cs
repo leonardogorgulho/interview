df53b06949f0a3e850d294a2d381aa164a63bdc4
Leonardo Gorgulho <leonardo.gorgulho@gmail.com>
2022-10-09 14:51:31 -0300

    Adding doc controller

---
+++ FinDox/FinDox.Api/Controllers/DocumentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace FinDox.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentController : Controller
+    {
+        [HttpPost("Upload")]
+        public async Task<ActionResult> UploadImage(IFormFile file)
+        {
+            string filePath = @$"D:\interviewFiles\{file.FileName}";
+            using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+
+                FileAttributes attributes = System.IO.File.GetAttributes(filePath);
+            }
+
+            return Ok();
+        }
+
+
+
+        [HttpGet]
+        [Route("Download/{url}")]
+        public async Task<IActionResult> Download(string url)
+        {
+            var bytes = await System.IO.File.ReadAllBytesAsync(url);
+            string contentType;
+
+            if (new FileExtensionContentTypeProvider().TryGetContentType(Path.GetFileName(url), out contentType))
+            {
+                return File(bytes, contentType, Path.GetFileName(url));
+            };
+
+            return File(bytes, "text/plain", Path.GetFileName(url));
+        }
+    }
+}
