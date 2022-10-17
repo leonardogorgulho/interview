1f7f2358ef82b86eeb767020166792296a397f5e
Leonardo Gorgulho <leonardo.gorgulho@gmail.com>
2022-10-17 14:35:27 -0300

    Making it unit

--- FinDox/FinDox.IntegrationTests/GroupControllerTests.cs
+++ FinDox/FinDox.IntegrationTests/GroupControllerTests.cs
@@ -8,64 +8,67 @@ namespace FinDox.IntegrationTests
 {
     public class GroupControllerTests : BaseTest
     {
-        private Group _group;
-
         [Test, Order(1)]
         public async Task Post_should_add_successfully_the_group()
         {
-            var expectedGroupName = DateTime.Now.ToString("yyyyMMddhhmmss");
+            _ = await PostGroup();
+        }
+
+        [Test, Order(2)]
+        public async Task Put_should_update_successfully_the_group()
+        {
+            var group = await PostGroup();
+
+            var expectedGroupName = Guid.NewGuid().ToString();
+
+            var httpResponse = await Client.PutAsJsonAsync($"/Group/{group.GroupId}", new GroupRequest
+            {
+                Name = expectedGroupName
+            });
+
+            group = JsonConvert.DeserializeObject<Group>(await httpResponse.Content.ReadAsStringAsync());
+
+            group.Should().NotBeNull();
+            group.GroupId.Should().BeGreaterThan(0);
+            group.Name.Should().Be(expectedGroupName);
+        }
+
+        [Test, Order(3)]
+        public async Task Get_should_retrieve_group()
+        {
+            var postedGroup = await PostGroup();
+
+            var group = await Client.GetFromJsonAsync<Group>($"/Group/{postedGroup.GroupId}");
+
+            group.Should().BeEquivalentTo(postedGroup);
+        }
+
+        [Test, Order(4)]
+        public async Task Delete_should_delete_successfully_group()
+        {
+            var postedGroup = await PostGroup();
+
+            var httpResponse = await Client.DeleteAsync($"/Group/{postedGroup.GroupId}");
+
+            httpResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        private async Task<Group> PostGroup()
+        {
+            var expectedGroupName = Guid.NewGuid().ToString();
 
             var httpResponse = await Client.PostAsJsonAsync("/Group", new GroupRequest
             {
                 Name = expectedGroupName
             });
 
-            _group = JsonConvert.DeserializeObject<Group>(await httpResponse.Content.ReadAsStringAsync());
+            var group = JsonConvert.DeserializeObject<Group>(await httpResponse.Content.ReadAsStringAsync());
 
-            _group.Should().NotBeNull();
-            _group.GroupId.Should().BeGreaterThan(0);
-            _group.Name.Should().Be(expectedGroupName);
-        }
+            group.Should().NotBeNull();
+            group.GroupId.Should().BeGreaterThan(0);
+            group.Name.Should().Be(expectedGroupName);
 
-        [Test, Order(2)]
-        public async Task Put_should_update_successfully_the_group()
-        {
-            var expectedGroupName = DateTime.Now.ToString("yyyyMMddhhmmss");
-
-            var httpResponse = await Client.PutAsJsonAsync($"/Group/{_group.GroupId}", new GroupRequest
-            {
-                Name = expectedGroupName
-            });
-
-            _group = JsonConvert.DeserializeObject<Group>(await httpResponse.Content.ReadAsStringAsync());
-
-            _group.Should().NotBeNull();
-            _group.GroupId.Should().BeGreaterThan(0);
-            _group.Name.Should().Be(expectedGroupName);
-        }
-
-        [Test, Order(3)]
-        public async Task Get_should_retrieve_group()
-        {
-            var group = await Client.GetFromJsonAsync<Group>($"/Group/{_group.GroupId}");
-
-            _group.Should().BeEquivalentTo(group);
-        }
-
-        [Test, Order(4)]
-        public async Task Delete_should_delete_successfully_group()
-        {
-            var httpResponse = await Client.DeleteAsync($"/Group/{_group.GroupId}");
-
-            httpResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
-        }
-
-        [Test, Order(5)]
-        public async Task Get_should_return_not_found()
-        {
-            var httpResponse = await Client.GetAsync($"/Group/{_group.GroupId}");
-
-            httpResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+            return group;
         }
     }
 }
--- FinDox/FinDox.IntegrationTests/UserControllerTests.cs
+++ FinDox/FinDox.IntegrationTests/UserControllerTests.cs
@@ -7,14 +7,75 @@ namespace FinDox.IntegrationTests
 {
     public class UserControllerTests : BaseTest
     {
-        UserResponse _user;
-
         [Test, Order(1)]
         public async Task Post_should_add_successfully_the_user()
         {
-            var uniqueData = DateTime.Now.ToString("yyyyMMddhhmmss");
+            _ = await PostUser();
+        }
+
+        [Test, Order(2)]
+        public async Task Put_should_update_successfully_the_user()
+        {
+            var postedUser = await PostUser();
+
+            var uniqueData = Guid.NewGuid().ToString();
             var expectedUserName = uniqueData;
-            var login = uniqueData;
+            var login = uniqueData.Substring(0, 10);
+            var password = "12345678901234567890123456789012";
+            var role = "R";
+
+            var httpResponse = await Client.PutAsJsonAsync($"/User/{postedUser.UserId}", new UserEntryRequest
+            {
+                Name = expectedUserName,
+                Login = login,
+                Password = password,
+                Role = role
+            });
+
+            var user = JsonConvert.DeserializeObject<UserResponse>(await httpResponse.Content.ReadAsStringAsync());
+
+            user.Should().NotBeNull();
+            user.UserId.Should().BeGreaterThan(0);
+            user.Name.Should().Be(expectedUserName);
+        }
+
+        [Test, Order(3)]
+        public async Task Get_should_retrieve_user()
+        {
+            var postedUser = await PostUser();
+
+            var user = await Client.GetFromJsonAsync<UserResponse>($"/User/{postedUser.UserId}");
+
+            postedUser.Should().BeEquivalentTo(user);
+        }
+
+        [Test, Order(4)]
+        public async Task GetUsers_should_retrieve_user()
+        {
+            var postedUser1 = await PostUser();
+            var postedUser2 = await PostUser();
+
+            var users = await Client.GetFromJsonAsync<List<UserResponse>>($"/User/list?skip=0&take=50");
+
+            postedUser1.Should().BeEquivalentTo(users.FirstOrDefault(d => d.UserId == postedUser1.UserId));
+            postedUser2.Should().BeEquivalentTo(users.FirstOrDefault(d => d.UserId == postedUser2.UserId));
+        }
+
+        [Test, Order(5)]
+        public async Task Delete_should_delete_successfully_user()
+        {
+            var postedUser = await PostUser();
+
+            var httpResponse = await Client.DeleteAsync($"/User/{postedUser.UserId}");
+
+            httpResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        private async Task<UserResponse> PostUser()
+        {
+            var uniqueData = Guid.NewGuid().ToString();
+            var expectedUserName = uniqueData;
+            var login = uniqueData.Substring(0, 10);
             var password = "12345678901234567890123456789012";
             var role = "R";
 
@@ -26,68 +87,13 @@ namespace FinDox.IntegrationTests
                 Role = role
             });
 
-            _user = JsonConvert.DeserializeObject<UserResponse>(await httpResponse.Content.ReadAsStringAsync());
+            var user = JsonConvert.DeserializeObject<UserResponse>(await httpResponse.Content.ReadAsStringAsync());
 
-            _user.Should().NotBeNull();
-            _user.UserId.Should().BeGreaterThan(0);
-            _user.Name.Should().Be(expectedUserName);
-        }
+            user.Should().NotBeNull();
+            user.UserId.Should().BeGreaterThan(0);
+            user.Name.Should().Be(expectedUserName);
 
-        [Test, Order(2)]
-        public async Task Put_should_update_successfully_the_user()
-        {
-            var uniqueData = DateTime.Now.ToString("yyyyMMddhhmmss");
-            var expectedUserName = uniqueData;
-            var login = uniqueData;
-            var password = "12345678901234567890123456789012";
-            var role = "R";
-
-            var httpResponse = await Client.PutAsJsonAsync($"/User/{_user.UserId}", new UserEntryRequest
-            {
-                Name = expectedUserName,
-                Login = login,
-                Password = password,
-                Role = role
-            });
-
-            _user = JsonConvert.DeserializeObject<UserResponse>(await httpResponse.Content.ReadAsStringAsync());
-
-            _user.Should().NotBeNull();
-            _user.UserId.Should().BeGreaterThan(0);
-            _user.Name.Should().Be(expectedUserName);
-        }
-
-        [Test, Order(3)]
-        public async Task Get_should_retrieve_user()
-        {
-            var user = await Client.GetFromJsonAsync<UserResponse>($"/User/{_user.UserId}");
-
-            _user.Should().BeEquivalentTo(user);
-        }
-
-        [Test, Order(4)]
-        public async Task GetUsers_should_retrieve_user()
-        {
-            var users = await Client.GetFromJsonAsync<List<UserResponse>>($"/User/list?skip=0&take=50");
-
-            var foundUser = users.FirstOrDefault(d => d.UserId == _user.UserId);
-            _user.Should().BeEquivalentTo(foundUser);
-        }
-
-        [Test, Order(5)]
-        public async Task Delete_should_delete_successfully_user()
-        {
-            var httpResponse = await Client.DeleteAsync($"/User/{_user.UserId}");
-
-            httpResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
-        }
-
-        [Test, Order(6)]
-        public async Task Get_should_return_not_found()
-        {
-            var httpResponse = await Client.GetAsync($"/User/{_user.UserId}");
-
-            httpResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+            return user;
         }
     }
 }
